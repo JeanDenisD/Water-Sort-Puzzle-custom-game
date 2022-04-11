1e76c76f0a88484a0249073151ecfc01f1afbe84
jidekickpush <jeandenisdrane@gmail.com>
2022-04-11 17:30:54 +0200

    function createdDomELement updated to create ul element in the DOM which represent the tubes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 function createDomElement(className){
     const board = document.getElementById("board");
-    const newElm = document.createElement("div");
+    const newElm = document.createElement("ul");
     newElm.className = className;
 
     board.appendChild(newElm);
@@ -8,13 +8,13 @@ function createDomElement(className){
     return newElm;
 }
 
-// function drawDomElement(instance){
-//     instance.domElement.style. ...
-// }
 
-const game = new Game(); //createDomElement, drawDomElement
+const game = new Game(createDomElement, drawDomElement);
 game.start();
 
 function drawDomElement(instance){
-    instance.domElement.style.backgroundColor = "black";
+    instance.domElement.style.width = instance + "%"
+    instance.domElement.style.heigth = instance + "%"
+    instance.document.style.backgroundColor = "black"
 }
+
